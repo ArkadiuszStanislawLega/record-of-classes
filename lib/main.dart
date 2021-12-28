da8b130e530a76c23e6a4747a7e505665e58a511
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-12-28 09:17:19 +0100

    Added objectbox to project.

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:record_of_classes/constants/app_urls.dart';
 import 'package:record_of_classes/widgets/pages/start_page.dart';
 
+import 'models/person.dart';
 
-void main() {
+import 'objectbox.g.dart'; // created by `flutter pub run build_runner build`
+
+class ObjectBox {
+  /// The Store of this app.
+  late final Store store;
+
+  ObjectBox._create(this.store) {
+    // Add any additional setup code, e.g. build queries.
+  }
+
+  /// Create an instance of ObjectBox to use throughout the app.
+  static Future<ObjectBox> create() async {
+    // Future<Store> openStore() {...} is defined in the generated objectbox.g.dart
+    final store = await openStore();
+    return ObjectBox._create(store);
+  }
+}
+
+late ObjectBox objectbox;
+
+Future<void> main() async {
+  // This is required so ObjectBox can get the application directory
+  // to store the database in.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  objectbox = await ObjectBox.create();
+
   runApp(const RecordOfClassesApp());
 }
 
-class RecordOfClassesApp extends StatelessWidget {
+
+
+class RecordOfClassesApp extends StatefulWidget {
   const RecordOfClassesApp({Key? key}) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() {
+    return _RecordOfClassesApp();
+  }
+}
 
+class _RecordOfClassesApp extends State<RecordOfClassesApp> {
   @override
   Widget build(BuildContext context) {
+    final personsBox = objectbox.store.box<Person>();
+
+    var length = personsBox.getAll().length;
+    var personFromDb = personsBox.get(1);
+    print(personFromDb!.name);
+
     return MaterialApp(
-      title: 'Rejestr zajęć',
+      title: 'Rejestr zajęć' + personFromDb.name,
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       initialRoute: AppUrls.HOME,
       routes: {
@@ -23,4 +64,5 @@ class RecordOfClassesApp extends StatelessWidget {
       },
     );
   }
+
 }
---
+++ test/db_test.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  test("Add persson to db", () async {
+    expect(length, 1);
+  });
+}
