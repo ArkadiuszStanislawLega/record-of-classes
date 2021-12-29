b9a2bd180ea0b5e0485b260681c45fced9c19a33
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-12-29 09:30:16 +0100

    Added template - add parents template.

--- lib/widgets/pages/create_student_page.dart
+++ lib/widgets/pages/create_student_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:objectbox/objectbox.dart';
 import 'package:record_of_classes/constants/strings.dart';
+import 'package:record_of_classes/models/account.dart';
 import 'package:record_of_classes/models/person.dart';
 import 'package:record_of_classes/models/student.dart';
 import 'package:record_of_classes/objectbox.g.dart';
+import 'package:record_of_classes/widgets/templates/add_parents_template.dart';
 
 import '../../main.dart';
 
@@ -29,59 +31,64 @@ class _CreateStudentPage extends State<CreateStudentPage> {
         title: const Text('Create student page'),
       ),
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: Strings.NAME,
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: Strings.NAME,
+              ),
+              onChanged: (userInput) => setState(
+                () {
+                  personName = userInput;
+                },
+              ),
             ),
-            onChanged: (userInput) => setState(
-              () {
-                personName = userInput;
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: Strings.SURNAME,
+              ),
+              onChanged: (userInput) => setState(
+                () {
+                  personSurname = userInput;
+                },
+              ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: Strings.AGE,
+              ),
+              onChanged: (userInput) => setState(
+                () {
+                  personAge = userInput;
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                createNewPerson();
+                Navigator.pop(
+                  context,
+                );
               },
+              child: const Text(Strings.CREATE_STUDENT),
             ),
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: Strings.SURNAME,
-            ),
-            onChanged: (userInput) => setState(
-              () {
-                personSurname = userInput;
-              },
-            ),
-          ),
-          TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: Strings.AGE,
-            ),
-            onChanged: (userInput) => setState(
-              () {
-                personAge = userInput;
-              },
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              createNewPerson();
-              Navigator.pop(
-                context,
-              );
-            },
-            child: const Text(Strings.CREATE_STUDENT),
-          ),
-        ],
-      )),
+            const AddParentsTemplate()
+          ],
+        ),
+      ),
     );
   }
 
   void createNewPerson() {
     var person = Person(name: personName, surname: personSurname);
     var student = Student(age: int.parse(personAge));
+    var account = Account();
+
     student.person.target = person;
+    account.student.target = student;
     _store.box<Student>().put(student);
   }
 
