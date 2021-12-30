1b892eeb2414e60b7fa4d8538da65b88324fceee
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-12-30 18:43:33 +0100

    Update students list item template - added remove item.

--- lib/widgets/templates/students_list_item_template.dart
+++ lib/widgets/templates/students_list_item_template.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:objectbox/objectbox.dart';
 import 'package:record_of_classes/constants/app_urls.dart';
+import 'package:record_of_classes/main.dart';
 import 'package:record_of_classes/models/student.dart';
 
 class StudentsListItemTemplate extends StatefulWidget {
-  const StudentsListItemTemplate({Key? key, required this.student})
-      : super(key: key);
   final Student student;
+  late Store _store;
+
+  StudentsListItemTemplate({Key? key, required this.student})
+      : super(key: key){
+    _store = objectBox.store;
+  }
+
 
   @override
   State<StatefulWidget> createState() {
@@ -15,21 +23,34 @@ class StudentsListItemTemplate extends StatefulWidget {
 }
 
 class _StudentsListItemTemplate extends State<StudentsListItemTemplate> {
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-        Text(
-          '${widget.student.person.target!.surname} ${widget.student.person.target!.name} ',
-          style: const TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold),
-        ),
-        Text(' lat: ${widget.student.age.toString()}')
-      ]),
-      onTap: () {
-        Navigator.pushNamed(context, AppUrls.DETAIL_STUDENT,
-            arguments: widget.student);
-      },
+
+    return Slidable(
+      actionPane: const SlidableDrawerActionPane(),
+      secondaryActions: [IconSlideAction(
+        caption: 'Delete',
+        color: Colors.red,
+        icon: Icons.delete,
+        onTap: () {
+          widget._store.box<Student>().remove(widget.student.id);
+        },
+      ), ],
+      child: ListTile(
+        title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+          Text(
+            '${widget.student.person.target!.surname} ${widget.student.person.target!.name} ',
+            style: const TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold),
+          ),
+          Text(' lat: ${widget.student.age.toString()}')
+        ]),
+        onTap: () {
+          Navigator.pushNamed(context, AppUrls.DETAIL_STUDENT,
+              arguments: widget.student);
+        },
+      ),
     );
   }
 }
