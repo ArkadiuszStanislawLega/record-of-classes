40a63abdcfb37111b3de5a94924b8d1ba98c7d78
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-01-05 17:40:12 +0100

    Update parent_of_student_liste_item_template.dart - code review.

--- lib/widgets/templates/parent_of_student_liste_item_template.dart
+++ lib/widgets/templates/parent_of_student_liste_item_template.dart
@@ -8,8 +8,10 @@ import 'package:record_of_classes/main.dart';
 import 'package:record_of_classes/models/parent.dart';
 import 'package:record_of_classes/models/student.dart';
 
-class ParentOfStudentListItemTemplate extends StatelessWidget{
-  ParentOfStudentListItemTemplate({Key? key, required this.parent, required this.student}) : super(key: key);
+class ParentOfStudentListItemTemplate extends StatelessWidget {
+  ParentOfStudentListItemTemplate(
+      {Key? key, required this.parent, required this.student})
+      : super(key: key);
   late Parent parent;
   late Student student;
   late Store _store;
@@ -25,57 +27,60 @@ class ParentOfStudentListItemTemplate extends StatelessWidget{
               caption: Strings.DISCONNECT,
               color: Colors.deepOrange,
               icon: Icons.remove,
-              onTap: (){
+              onTap: () {
                 removeParent();
-                var parentValues = '${parent.person.target!.surname} ${parent.person.target!.name}';
-                var studentValues = '${student.person.target!.surname} ${student.person.target!.name}';
-
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('$parentValues i $studentValues nie są już rodziną!'),
-                    duration: const Duration(milliseconds: 1500),
-                    width: 280.0,
-                    // Width of the SnackBar.
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 15.0, // Inner padding for SnackBar content.
-                    ),
-                    behavior: SnackBarBehavior.floating,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                );
+                showInformationSnackBar(context);
               }),
         ],
         child: ListTile(
-            title: Column(children: personData()), onTap: (){    Navigator.pushNamed(context, AppUrls.DETAIL_PARENT,
-            arguments: parent);}),
-
+            title: Column(children: personData()),
+            onTap: () {
+              Navigator.pushNamed(context, AppUrls.DETAIL_PARENT,
+                  arguments: parent);
+            }),
       );
     }
     return const Text('');
   }
 
+  void showInformationSnackBar(var context) {
+    var parentValues =
+        '${parent.person.target!.surname} ${parent.person.target!.name}';
+    var studentValues =
+        '${student.person.target!.surname} ${student.person.target!.name}';
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$parentValues i $studentValues nie są już rodziną!'),
+        duration: const Duration(milliseconds: 1500),
+        width: 280.0,
+        // Width of the SnackBar.
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15.0, // Inner padding for SnackBar content.
+        ),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+
   void removeParent() {
-    var box2 = _store.box<Parent>();
-    parent.children.removeWhere((w) => w.id == student.id);
-    box2.put(parent);
-
-
-    var box = _store.box<Student>();
-    student.parents.remove(parent);
-    box.put(student);
+    parent.children.removeWhere((s) => s.id == student.id);
+    student.parents.removeWhere((p) => p.id == parent.id);
+    _store.box<Parent>().put(parent);
+    _store.box<Student>().put(student);
   }
 
   List<Widget> personData() {
     var person = parent.person.target!;
     return [
-      Text('${person.surname} ${person.name} ',
+      Text(
+        '${person.surname} ${person.name} ',
         style: const TextStyle(
             color: Colors.blueGrey, fontWeight: FontWeight.bold),
       ),
     ];
   }
-
-
 }
