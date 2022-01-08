c37a981a7074e36734e02efd17d5020c2e6dbb8a
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-01-08 10:26:07 +0100

    Update detail group page - added edit mode.

--- lib/widgets/pages/detail_group_page.dart
+++ lib/widgets/pages/detail_group_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:record_of_classes/constants/strings.dart';
 import 'package:record_of_classes/models/group.dart';
 
 class DetailGroupPage extends StatefulWidget {
@@ -10,20 +11,45 @@ class DetailGroupPage extends StatefulWidget {
 
 class _DetailGroupPageState extends State<DetailGroupPage> {
   late Group group;
+  bool _isEditModeEnabled = false;
 
   @override
   Widget build(BuildContext context) {
     group = ModalRoute.of(context)!.settings.arguments as Group;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(group.name),
-      ),
-      body: Column(
-        children: [
-          Text('Typ zajęć: ${group.classesType.target!.name}'),
-          Text('Adres zajęć: ${group.address.target.toString()}'),
-        ],
-      ),
+        appBar: AppBar(
+          title: Text(group.name),
+        ),
+        body: _isEditModeEnabled ? _editModeEnabled() : _editModeDisabled());
+  }
+
+  Widget _editModeEnabled() {
+    return Column(
+      children: [
+        TextButton(
+            onPressed: () {
+              _setEditModeDisable();
+            },
+            child: const Text(Strings.OK))
+      ],
     );
   }
+
+  Widget _editModeDisabled() {
+    return Column(
+      children: [
+        Text('Typ zajęć: ${group.classesType.target!.name}'),
+        Text('Adres zajęć: ${group.address.target.toString()}'),
+        TextButton(
+            onPressed: () {
+              _setEditModeEnable();
+            },
+            child: const Text(Strings.EDIT)),
+      ],
+    );
+  }
+
+  void _setEditModeEnable() => setState(() => _isEditModeEnabled = true);
+
+  void _setEditModeDisable() => setState(() => _isEditModeEnabled = false);
 }
