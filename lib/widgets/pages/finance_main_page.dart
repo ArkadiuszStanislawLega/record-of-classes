0a8a0d842b5d57ffa9a2f3e38ecfec94432fc42e
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-01-15 13:33:50 +0100

    Update finance main page - added filtering list.

--- lib/widgets/pages/finance_main_page.dart
+++ lib/widgets/pages/finance_main_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:objectbox/objectbox.dart';
 import 'package:record_of_classes/constants/strings.dart';
+import 'package:record_of_classes/main.dart';
+import 'package:record_of_classes/models/bill.dart';
 import 'package:record_of_classes/widgets/templates/lists/all_unpaid_bills_list_template.dart';
 
 class FinanceMainPage extends StatefulWidget {
@@ -10,15 +13,59 @@ class FinanceMainPage extends StatefulWidget {
 }
 
 class _FinanceMainPageState extends State<FinanceMainPage> {
+  late Store _store;
+  late Stream<List<Bill>> _billsStream;
+  bool _isPaiedFilter = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(Strings.FINANCE),
       ),
-      body: Column(
-        children: const [AllUnpaidBillsTemplate()],
+      body: StreamBuilder<List<Bill>>(
+        stream: _billsStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var unpaid =
+                snapshot.data!.skipWhile((value) => value.isPaid).toList();
+            var paid =
+                snapshot.data!.skipWhile((value) => !value.isPaid).toList();
+            return Column(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_isPaiedFilter) {
+                        _isPaiedFilter = false;
+                      } else {
+                        _isPaiedFilter = true;
+                      }
+                    });
+                  },
+                  child: Text('Zmień'),
+                ),
+                Text('Zapłaconych zajęć: ${paid.length.toString()}'),
+                Text('Nie zapłaconych zajęć: ${unpaid.length.toString()}'),
+                AllUnpaidBillsTemplate(bills: _isPaiedFilter ? paid : unpaid),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _store = objectBox.store;
+    _billsStream = _store
+        .box<Bill>()
+        .query()
+        .watch(triggerImmediately: true)
+        .map((query) => query.find());
   }
 }
--- lib/widgets/templates/lists/all_unpaid_bills_list_template.dart
+++ lib/widgets/templates/lists/all_unpaid_bills_list_template.dart
@@ -1,50 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:objectbox/objectbox.dart';
-import 'package:record_of_classes/main.dart';
 import 'package:record_of_classes/models/bill.dart';
 import 'package:record_of_classes/widgets/templates/list_items/bill_list_item.dart';
 
 class AllUnpaidBillsTemplate extends StatefulWidget {
-  const AllUnpaidBillsTemplate({Key? key}) : super(key: key);
+  AllUnpaidBillsTemplate({Key? key, required this.bills}) : super(key: key);
+
+  late List<Bill> bills;
 
   @override
   _AllUnpaidBillsTemplateState createState() => _AllUnpaidBillsTemplateState();
 }
 
 class _AllUnpaidBillsTemplateState extends State<AllUnpaidBillsTemplate> {
-  late Store _store;
-  late Stream<List<Bill>> _billsStream;
-
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<Bill>>(
-      stream: _billsStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          var unpaied = snapshot.data?.skipWhile((value) => value.isPaid);
-          return ListView.builder(
-            itemCount: unpaied?.length,
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              return BillListItem(bill: unpaied!.elementAt(index));
-            },
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
+    return ListView.builder(
+      itemCount: widget.bills.length,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return BillListItem(bill: widget.bills.elementAt(index));
       },
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _store = objectBox.store;
-    _billsStream = _store
-        .box<Bill>()
-        .query()
-        .watch(triggerImmediately: true)
-        .map((query) => query.find());
   }
 }
