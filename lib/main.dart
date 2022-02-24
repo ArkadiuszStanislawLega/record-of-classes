f5e640aafdbeda1c38015ed74e13fcdad7c24583
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-02-24 19:14:23 +0100

    added Import database.

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,10 @@ class ObjectBox {
     });
     return ObjectBox._create(store);
   }
+
+  void open(){
+    store.syncClient();
+  }
 }
 
 late ObjectBox objectBox;
--- lib/widgets/pages/manage_database_page.dart
+++ lib/widgets/pages/manage_database_page.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:file_picker_cross/file_picker_cross.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_share/flutter_share.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record_of_classes/constants/app_strings.dart';
+import 'package:record_of_classes/main.dart';
 
 class ManageDatabasePage extends StatefulWidget {
   const ManageDatabasePage({Key? key}) : super(key: key);
@@ -15,16 +18,15 @@ class ManageDatabasePage extends StatefulWidget {
 }
 
 class _ManageDatabasePageState extends State<ManageDatabasePage> {
+  String _path = '', _length = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           AppStrings.MANAGE_DATABASE,
-          style: Theme
-              .of(context)
-              .textTheme
-              .headline1,
+          style: Theme.of(context).textTheme.headline1,
         ),
       ),
       body: Center(
@@ -32,6 +34,8 @@ class _ManageDatabasePageState extends State<ManageDatabasePage> {
           padding: const EdgeInsets.all(10.0),
           child: Column(
             children: [
+              Text(_path),
+              Text(_length),
               TextButton(
                 child: const Text(AppStrings.EXPORT_DATABASE),
                 onPressed: exportDb,
@@ -49,39 +53,59 @@ class _ManageDatabasePageState extends State<ManageDatabasePage> {
 
   Future<void> exportDb() async {
     await getApplicationDocumentsDirectory().then(
-          (dir) {
+      (dir) {
         FlutterShare.shareFile(
           title: AppStrings.EXPORTING_DATABASE_TITLE,
-          text: AppStrings.EXPORTING_DATABASE_TEXT,
           filePath: '${dir.path}/${AppStrings.DATABASE_NAME}',
         );
       },
     );
   }
 
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    print('path ${path}');
+    return File('$path/${AppStrings.DATABASE_NAME}/data.mdb');
+  }
+
+  Future<int> deleteFile() async {
+    try {
+      final file = await _localFile;
+      print('usuwam: $file');
+      await file.delete();
+    } catch (e) {
+      return 0;
+    }
+    return 0;
+  }
+
   Future<void> importDb() async {
-    await getApplicationDocumentsDirectory().then(
-          (dir) async {
-            FilePickerCross myFile = await FilePickerCross.importFromStorage(
-              type: FileTypeCross.any,
-            );
-            myFile.saveToPath(path: '${dir.path}/${AppStrings.DATABASE_NAME}');
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final objectBoxDirectory = Directory(
+        documentsDirectory.path + '/${AppStrings.DATABASE_NAME}/data.mdb');
+
+    FilePickerCross myFile = await FilePickerCross.importFromStorage(
+      type: FileTypeCross.any,
+    );
+
+
+    final dbFile = File(objectBoxDirectory.path);
+
+    ByteData data = await rootBundle.load("${myFile.path}");
+
+    dbFile.writeAsBytes(data.buffer.asUint8List());
+
+    setState(
+      () {
+        _length = myFile.length.toString();
+        _path = myFile.path!;
       },
     );
   }
-
-
-// String? outputFile = await FilePicker.platform.saveFile(
-//   dialogTitle: 'Please select an output file:',
-//   fileName: AppStrings.DATABASE_NAME,
-// );
-
-// FilePickerResult? result = await FilePicker.platform.pickFiles(
-//     allowMultiple: true);
-//
-// if (result != null) {
-//   List<File> files = result.paths.map((path) => File(path!)).toList();
-// } else {
-//   // User canceled the picker
-// }
 }
