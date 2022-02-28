8d750e99dc91e272c737ae66e974d2f8f34828af
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-02-28 19:44:23 +0100

    Update create_student_template.dart - code review - change style.

--- lib/widgets/templates/create/create_student_template.dart
+++ lib/widgets/templates/create/create_student_template.dart
@@ -5,33 +5,24 @@ import 'package:record_of_classes/models/account.dart';
 import 'package:record_of_classes/models/person.dart';
 import 'package:record_of_classes/models/student.dart';
 import 'package:record_of_classes/widgets/templates/create/create_person_template.dart';
+import 'package:record_of_classes/widgets/templates/text_field_template_num.dart';
 
 class CreateStudentTemplate extends StatelessWidget {
   CreateStudentTemplate({Key? key}) : super(key: key);
-  String _personAge = '';
   late Student _createdStudent;
   late Person _createdPerson;
   late Account _account;
   final _createPersonTemplate = CreatePersonTemplate();
-  final TextEditingController _ageController = TextEditingController();
+  late TextFieldTemplateNum _getAge;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.all(10.0),
       child: Column(
         children: [
           _createPersonTemplate,
-          TextField(
-              controller: _ageController,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                label: Text(
-                  AppStrings.AGE,
-                  style: Theme.of(context).textTheme.headline2,
-                ),
-              ),
-              onChanged: (userInput) => _personAge = userInput),
+          _getAge = TextFieldTemplateNum(label: AppStrings.AGE, hint: '')
         ],
       ),
     );
@@ -39,7 +30,7 @@ class CreateStudentTemplate extends StatelessWidget {
 
   Student getStudent() {
     _createdPerson = _createPersonTemplate.getPerson();
-    _createdStudent = Student(age: int.parse(_personAge));
+    _createdStudent = Student(age: int.parse(_getAge.input));
     _account = Account()..student.target = _createdStudent;
     _createdStudent.account.target = _account;
     _createdPerson.dbPersonType = PersonType.student.index;
@@ -49,11 +40,10 @@ class CreateStudentTemplate extends StatelessWidget {
   }
 
   bool isValuesAreValid() =>
-      int.parse(_personAge) > 0 && _createdPerson.name != '';
+      int.parse(_getAge.input) > 0 && _createdPerson.name != '';
 
   void clearFields() {
-    _personAge = '';
-    _ageController.clear();
+    _getAge.clear();
     _createPersonTemplate.clearTemplate();
   }
 }
