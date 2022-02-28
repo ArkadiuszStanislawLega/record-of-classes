e0d7114755fd7453190f3889460ea53d9980ff99
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-02-28 19:38:23 +0100

    Update create_person_template.dart - code review - change style.

--- lib/widgets/templates/create/create_person_template.dart
+++ lib/widgets/templates/create/create_person_template.dart
@@ -2,11 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:record_of_classes/constants/app_strings.dart';
 import 'package:record_of_classes/models/person.dart';
+import 'package:record_of_classes/widgets/templates/text_field_template.dart';
 
 class CreatePersonTemplate extends StatelessWidget {
-  String _personName = '', _personSurname = '';
-  final TextEditingController _nameController = TextEditingController(),
-      _surnameController = TextEditingController();
+  late TextFieldTemplate _personName, _personSurname;
 
   CreatePersonTemplate({Key? key}) : super(key: key);
 
@@ -14,38 +13,21 @@ class CreatePersonTemplate extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        TextField(
-            controller: _nameController,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              label: Text(
-                AppStrings.NAME,
-                style: Theme.of(context).textTheme.headline2,
-              ),
-            ),
-            onChanged: (userInput) => _personName = userInput),
-        TextField(
-            controller: _surnameController,
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              label: Text(
-                AppStrings.SURNAME,
-                style: Theme.of(context).textTheme.headline2,
-              ),
-            ),
-            onChanged: (userInput) => _personSurname = userInput),
+        _personName = TextFieldTemplate(
+          label: AppStrings.NAME,
+          hint: '',
+        ),
+        _personSurname = TextFieldTemplate(label: AppStrings.SURNAME, hint: '')
       ],
     );
   }
 
   void clearTemplate() async {
-    _personName = '';
-    _personSurname = '';
-    _surnameController.clear();
-    _nameController.clear();
+    _personName.clear();
+    _personSurname.clear();
   }
 
   Person getPerson() {
-    return Person(name: _personName, surname: _personSurname);
+    return Person(name: _personName.input, surname: _personSurname.input);
   }
 }
