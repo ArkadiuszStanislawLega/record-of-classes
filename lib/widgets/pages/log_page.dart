40bd6738297a429ea60e79c5c7457702c86f72c8
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-03-23 21:26:55 +0100

    Update log page - code review - added filtering depends on student surname.

--- lib/widgets/pages/log_page.dart
+++ lib/widgets/pages/log_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:record_of_classes/constants/app_strings.dart';
+import 'package:record_of_classes/enumerators/ModelType.dart';
 import 'package:record_of_classes/main.dart';
 import 'package:record_of_classes/models/log.dart';
+import 'package:record_of_classes/models/person.dart';
 import 'package:record_of_classes/widgets/templates/list_items/log_list_item_template.dart';
+
+import '../../objectbox.g.dart';
 
 class LogPage extends StatefulWidget {
   const LogPage({Key? key}) : super(key: key);
@@ -12,11 +16,29 @@ class LogPage extends StatefulWidget {
 }
 
 class _LogPageState extends State<LogPage> {
+  String _input = '';
+  List<Log> _logs = [];
+
   @override
   Widget build(BuildContext context) {
+    _prepareList();
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppStrings.operationHistory, style: Theme.of(context).textTheme.headline1,),
+        bottom: PreferredSize(
+          preferredSize: const Size(0, 10),
+          child: TextField(
+            style: Theme.of(context).textTheme.headline2,
+            onChanged: (input) {
+              setState(() {
+                _input = input;
+              });
+            },
+          ),
+        ),
+        title: Text(
+          AppStrings.operationHistory,
+          style: Theme.of(context).textTheme.headline1,
+        ),
       ),
       body: Center(
         child: _list(),
@@ -24,16 +46,60 @@ class _LogPageState extends State<LogPage> {
     );
   }
 
+  void _prepareList() {
+    _input == '' ? _emptyFilterInput() : _getLogsDependsOnStudentSurname();
+  }
+
+  void _emptyFilterInput() {
+    setState(() {
+      if (_logs.isNotEmpty) {
+        _logs.clear();
+      }
+      _logs = ObjectBox.store.box<Log>().getAll();
+      _logs.sort(
+          (firstLog, secondLog) => firstLog.date.compareTo(secondLog.date));
+    });
+  }
+
   Widget _list() {
-    List<Log> logs = ObjectBox.store.box<Log>().getAll();
-    logs.sort((firstLog, secondLog) => firstLog.date.compareTo(secondLog.date));
     return ListView.builder(
       reverse: true,
       padding: const EdgeInsets.all(8),
-      itemCount: logs.length,
+      itemCount: _logs.length,
       itemBuilder: (BuildContext context, int index) {
-        return LogListItemTemplate(log: logs[index]);
+        return LogListItemTemplate(log: _logs[index]);
       },
     );
+  }
+
+  void _getLogsDependsOnStudentSurname() {
+    List<Person> persons = ObjectBox.store
+        .box<Person>()
+        .query(
+            Person_.surname.contains(_input)..and(Person_.personType.equals(2)))
+        .build()
+        .find();
+
+    setState(() {
+      _logs.clear();
+
+      for (var person in persons) {
+        List<Log> personLogs = ObjectBox.store
+            .box<Log>()
+            .query(Log_.modelType.equals(ModelType.account.index)
+              ..and(Log_.participatingClassId
+                  .equals(person.student.target!.account.target!.id)))
+            .build()
+            .find();
+        for (var element in personLogs) {
+          if (element.participatingClassId ==
+              person.student.target!.account.targetId) {
+            _logs.add(element);
+          }
+        }
+      }
+      _logs
+          .sort((firsLog, secondLog) => firsLog.date.compareTo(secondLog.date));
+    });
   }
 }
